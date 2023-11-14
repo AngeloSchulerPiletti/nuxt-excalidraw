d76cc462199e9cb347ba50a86044ffb3f8b780cd
angelo <angelopiletti@gmail.com>
2023-11-15 00:34:21 +0100

    feat: :sparkles: add minimal example on playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,10 @@
+<script setup lang="ts">
+</script>
+
 <template>
   <div>
-    Nuxt module playground!
+    <ClientOnly>
+      <ExcalidrawBoard />
+    </ClientOnly>
   </div>
 </template>
-
-<script setup>
-</script>
