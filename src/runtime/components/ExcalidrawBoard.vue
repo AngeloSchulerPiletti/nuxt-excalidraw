647f6c7e3fb09d647e6a35bb834fed344a0074ab
angelo <angelopiletti@gmail.com>
2023-11-15 00:35:37 +0100

    feat: :sparkles: create excalidraw board component

---
+++ src/runtime/components/ExcalidrawBoard.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import { Excalidraw } from '@excalidraw/excalidraw'
+import React from 'react'
+import { type Root, createRoot } from 'react-dom/client'
+import type { Ref } from 'vue'
+import type { ExcalidrawProps } from '@excalidraw/excalidraw/types/types'
+
+export interface Props extends ExcalidrawProps {}
+
+const props = defineProps<Props>()
+
+const excalidrawRef: Ref<HTMLDivElement | null> = ref(null)
+let root: null | Root = null
+
+onMounted(() => {
+  if (excalidrawRef.value) {
+    root = createRoot(excalidrawRef.value)
+    root.render(React.createElement(Excalidraw, props as any))
+  }
+})
+
+onBeforeUnmount(() => {
+  if (root)
+    root.unmount()
+})
+</script>
+
+<template>
+  <div ref="excalidrawRef" class="excalidraw-board" />
+</template>
+
+<style scoped>
+.excalidraw-board {
+  min-height: 100%;
+  height: 90dvh;
+}
+</style>
